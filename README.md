81f3e05b79f0ede2bf5adb39ea84a21d7dab5560
mankej <github@msiwak.eu>
2023-04-05 19:33:57 +0200

    init

---
+++ README.md
@@ -0,0 +1,114 @@
+# ITHM
+**ITHM** stands for **IntelliJ's Terminal History Manager** and was created as workaround for the lack of native 
+support for terminal history management per project in JetBrains IDE, which often leads to loss or mixing after 
+every major update.
+More information can be found here:
+https://youtrack.jetbrains.com/issue/IDEA-118659/project-specific-terminal-history
+
+I'm using it in PHPStorm/Ubuntu/Bash on a daily basis and haven't tested it with any other IDEs yet, but since it 
+depends on the global IntelliJ flow, it should work in any JetBrains IDE.
+
+## Install
+
+#### METHOD 1
+copy `ithm.sh` to the home directory
+
+in `~/.bashrc` add the following line
+``` 
+source ./ithm.sh
+```
+#### METHOD 2
+copy and paste contents of ithm.sh file in `~/.bashrc`
+
+## Configuration
+
+> History config
+> by default, ITHM sets up history options like that:
+```
+shopt -s histappend
+
+PROMPT_COMMAND="history -n; history -a; history -w; $PROMPT_COMMAND"
+```
+> If you want to disable it, set the ITHM_DISABLE_HISTORY_SETUP environment variable to 1 in terminal settings.
+
+To start working, ITHM needs one environment variable ITHM_PROJECT_ROOT set to the project root.
+To set it up, follow these steps:
+
+> file -> settings -> tools -> terminal -> environment variables -> 
+> click list icon on input or click input and shift + enter -> click + icon(add) 
+
+> ITHM_PROJECT_ROOT = $PROJECT_DIR$ - special IntelliJ's variable pointing to project root
+
+> after setting env you need to restart IDE or only new terminals will start with enabled ITHM.
+
+
+## OTHER CONFIG ENVS
+#### ITHM_GLOBAL_HISTORY_FILE
+allows defining global history file(one you use outside the IDE) to use in functions like `ithm_import`/`ithm_export` which lets you merge history files
+> **default:** ITHM_GLOBAL_HISTORY_FILE:=$HISTFILE
+#### ITHM_DIRECTORY_BROWSER
+sets the command to open directories
+> **default**: xdg-open
+#### ITHM_FILE_EDITOR=nano
+sets the command to edit files
+> **default**: nano
+#### ITHM_FILE_READER
+sets the command to read files
+> **default**: less
+#### ITHM_HISTORY_FILE_PATH
+The path where history files should be stored. For example, you can use it to set config per IDE.
+> **default**:  ITHM_PROJECT_ROOT/.idea/terminal/history/
+
+
+### ITHM_INITAL_USE_GLOBAL_HISTORY
+setting to 1 causes the terminal to start with the global history. Don't set if you want to have an empty history in new terminal.
+> **default**: disabled
+#### ITHM_BACKUP_ON_CLOSE
+setting to 1 enables backup creation when the terminal is closed
+> **default**: disabled
+#### ITHM_APPEND_SESSION_HISTORY_TO_GLOABL_ON_CLOSE
+setting to 1 enables appending current IDE session history to global file defined in `ITHM_GLOBAL_HISTORY_FILE`  when the terminal is closed
+> **default**: disabled
+#### ITHM_APPEND_SESSION_HISTORY_TO_FILE_ON_CLOSE
+setting to file path enables appending current IDE session history to this file when the terminal is closed
+> **default**: disabled
+#### ITHM_SAVE_HISTORY_ON_CLOSE_AND_RESET_HISTFILE
+setting to 1 enables saving history and restoring `HISTFILE` to value from `ITHM_GLOBAL_HISTORY_FILE`
+> **default**: disabled
+
+## IN TERMINAL FUNCTIONS
+#### ithm_current
+shows current history file name
+#### ithm_directory
+shows current director path where history files are stored
+#### ithm_list
+shows all available history files in current project
+#### ithm_use $CURRENT_PROJECT_HISTORY_FILE_NAME
+allows you to use other history file chosen from available files in current project. Content of source file is copied to target file and source is deleted.
+Useful when accidentally close terminal and want to restore previous history.
+#### ithm_clone $CURRENT_PROJECT_HISTORY_FILE_NAME
+like `ithm_use` allows to use other history file contents from current project but after copy source file is not deleted. 
+useful when you want init terminal with history from other terminal.
+#### ithm_import $FILE=global/intellij/file_path
+like `ithm_clone` but allow any file contents to be appended not only from current project. 
+You can use "global" keyword to append global history or "intellij" to append native history file. Keyword "intellij" is especially useful when you start using ITHM in project with existing terminal history. 
+#### ithm_export $FILE=global/file_path
+allow to append current terminal history to any file. You can use "global" keyword to append to global history file.
+#### ithm_read $CURRENT_PROJECT_HISTORY_FILE_NAME(optional)=intellij/file_path
+allow to read current project history file contents using reader command set in `ITHM_FILE_READER`. Not setting arg = use current file.
+You can use "intellij" keyword to read native history file.
+#### ithm_edit $CURRENT_PROJECT_HISTORY_FILE_NAME(optional)
+allow to edit current project history file using editor command set in `ITHM_FILE_EDITOR`. Not setting arg = use current file.
+#### ithm_reload
+clear current history and reload current history file.
+#### ithm_backup $CURRENT_PROJECT_HISTORY_FILE_NAME(optional)
+creates backup for current project history file. Not setting arg = use current file.
+#### ithm_backup_list $CURRENT_PROJECT_HISTORY_FILE_NAME(optional)
+lists all backups for current project history file. Not setting arg = use current file.
+#### ithm_backup_read $CURRENT_PROJECT_HISTORY_FILE_NAME(optional)
+`ithm_read` but for backup files. Not setting arg = use current file.
+#### ithm_backup_restore $CURRENT_PROJECT_HISTORY_FILE_NAME $BACKUP_FILE
+restore current project history file from chosen backup file.
+#### ithm_open $DEST=intellij(optional)
+opens directory where current history file is stored using command set in `ITHM_DIRECTORY_BROWSER`.
+You can use "intellij" keyword to open directory where native history files are stored.
---
+++ ithm.sh
@@ -0,0 +1,224 @@
+#INTELLIJ TERMINAL HISTORY MANAGER
+if [ -n "$__INTELLIJ_COMMAND_HISTFILE__" ] && [ -n "$ITHM_PROJECT_ROOT" ]; then
+
+    if [ -z "$ITHM_DISABLE_HISTORY_SETUP" ]; then
+        shopt -s histappend
+
+        PROMPT_COMMAND="history -n; history -a; history -w; $PROMPT_COMMAND"
+    fi
+    ITHM_HISTORY_FILE_NAME=$(echo "$__INTELLIJ_COMMAND_HISTFILE__" | grep -Eoh "([^/]+?)$" | rev | cut -d '-' -f 1 | rev)
+    PROMPT_COMMAND="echo -n [$ITHM_HISTORY_FILE_NAME]; $PROMPT_COMMAND"
+    INTELLIJ_ORIGINAL_PATH=$__INTELLIJ_COMMAND_HISTFILE__
+    unset __INTELLIJ_COMMAND_HISTFILE__
+    ITHM_GLOBAL_HISTORY_FILE=${ITHM_GLOBAL_HISTORY_FILE:=$HISTFILE}
+    ITHM_DIRECTORY_BROWSER=${ITHM_DIRECTORY_BROWSER:=xdg-open}
+    ITHM_FILE_EDITOR=${ITHM_FILE_EDITOR:=nano}
+    ITHM_FILE_READER=${ITHM_FILE_READER:=less}
+    ITHM_HISTORY_FILE_PATH=${ITHM_HISTORY_FILE_PATH:=$ITHM_PROJECT_ROOT/.idea/terminal/history/}
+    ITHM_HISTORY_FILE="$ITHM_HISTORY_FILE_PATH$ITHM_HISTORY_FILE_NAME"
+    alias ithm_current='echo $ITHM_HISTORY_FILE_NAME'
+    alias ithm_directory='echo $ITHM_HISTORY_FILE_PATH'
+    alias ithm_list="find $ITHM_HISTORY_FILE_PATH -maxdepth 1 -type f -and -not -name '$ITHM_HISTORY_FILE_NAME' -printf '%f\n'"
+    if [ -n "$ITHM_BACKUP_ON_CLOSE" ]; then
+        trap "ihm_backup" EXIT
+    fi
+    if [ -n "$ITHM_APPEND_SESSION_HISTORY_TO_GLOABL_ON_CLOSE" ]; then
+        trap "$(builtin printf 'history -a %q' "$ITHM_GLOBAL_HISTORY_FILE")" EXIT
+    fi
+    if [ -n "$ITHM_APPEND_SESSION_HISTORY_TO_FILE_ON_CLOSE" ]; then
+        trap "$(builtin printf 'history -a %q' "$ITHM_APPEND_SESSION_HISTORY_TO_FILE_ON_CLOSE")" EXIT
+    fi
+    if [ -n "$ITHM_SAVE_HISTORY_ON_CLOSE_AND_RESET_HISTFILE" ]; then
+        trap "$(builtin printf 'history -w %q; HISTFILE=%q' "$ITHM_HISTORY_FILE_NAME" "$ITHM_GLOBAL_HISTORY_FILE")" EXIT
+    fi
+    ithm_clone() {
+        if [ -n "$1" ] && [ -f "$ITHM_HISTORY_FILE_PATH$1" ] && [ "$1" != "$ITHM_HISTORY_FILE_NAME" ]; then
+            while true; do
+                read -p "clone $1 to $ITHM_HISTORY_FILE_NAME?" yn
+                case $yn in
+                y)
+                    history -c
+                    history -r "$ITHM_HISTORY_FILE_PATH$1"
+                    history -w
+                    echo "done"
+                    break
+                    ;;
+                n)
+                    break
+                    ;;
+                *)
+                    echo "Answer 'y' or 'n'"
+                    ;;
+                esac
+            done
+        fi
+    }
+    ithm_import() {
+        local ACTION=$1
+        local FILE=$ITHM_HISTORY_FILE_PATH$1
+        if [ "$ACTION" = "global" ]; then
+            ACTION=$ITHM_GLOBAL_HISTORY_FILE
+            FILE=$ITHM_GLOBAL_HISTORY_FILE
+        fi
+        if [ "$ACTION" = "intellij" ]; then
+            ACTION=$INTELLIJ_ORIGINAL_PATH
+            FILE=$INTELLIJ_ORIGINAL_PATH
+        fi
+
+        if [ -n "$1" ] && [ -f "$FILE" ] && [ "$1" != "$ITHM_HISTORY_FILE_NAME" ]; then
+            while true; do
+                read -p "append $ACTION to $ITHM_HISTORY_FILE_NAME?" yn
+                case $yn in
+                y)
+                    history -n $FILE
+                    history -w
+                    echo "done"
+                    break
+                    ;;
+                n)
+                    break
+                    ;;
+                *)
+                    echo "Answer 'y' or 'n'"
+                    ;;
+                esac
+            done
+        fi
+    }
+    ithm_export() {
+        local ACTION=$1
+        local FILE=$ITHM_HISTORY_FILE_PATH$1
+        if [ "$ACTION" = "global" ]; then
+            ACTION=$ITHM_GLOBAL_HISTORY_FILE
+            FILE=$ITHM_GLOBAL_HISTORY_FILE
+        fi
+
+        if [ -n "$1" ] && [ -f "$FILE" ] && [ "$1" != "$ITHM_HISTORY_FILE_NAME" ]; then
+            while true; do
+                read -p "append $ITHM_HISTORY_FILE_NAME to $ACTION?" yn
+                case $yn in
+                y)
+                    history -n $FILE
+                    echo "done"
+                    break
+                    ;;
+                n)
+                    break
+                    ;;
+                *)
+                    echo "Answer 'y' or 'n'"
+                    ;;
+                esac
+            done
+        fi
+    }
+    ithm_use() {
+        if [ -n "$1" ] && [ -f "$ITHM_HISTORY_FILE_PATH$1" ] && [ "$1" != "$ITHM_HISTORY_FILE_NAME" ]; then
+            while true; do
+                read -p "copy $1 to $ITHM_HISTORY_FILE_NAME and remove?" yn
+                case $yn in
+                y)
+                    history -c
+                    history -r "$ITHM_HISTORY_FILE_PATH$1"
+                    history -w
+                    rm "$ITHM_HISTORY_FILE_PATH$1"
+                    echo "done"
+                    break
+                    ;;
+                n)
+                    break
+                    ;;
+                *)
+                    echo "Answer 'y' or 'n'"
+                    ;;
+
+                esac
+            done
+        fi
+    }
+    ithm_read() {
+        if [ -z "$1" ]; then
+            $ITHM_FILE_READER "$ITHM_HISTORY_FILE"
+        fi
+        if [ "$1" = "intellij" ]; then
+            $ITHM_FILE_READER "$INTELLIJ_ORIGINAL_PATH"
+        fi
+        if [ -n "$1" ] && [ -f "$ITHM_HISTORY_FILE_PATH$1" ]; then
+            $ITHM_FILE_READER "$ITHM_HISTORY_FILE_PATH$1"
+        fi
+    }
+    ithm_edit() {
+        if [ -z "$1" ]; then
+            $ITHM_FILE_EDITOR "$ITHM_HISTORY_FILE"
+            ithm_reload
+        fi
+        if [ -n "$1" ] && [ -f "$ITHM_HISTORY_FILE_PATH$1" ]; then
+            $ITHM_FILE_EDITOR "$ITHM_HISTORY_FILE_PATH$1"
+        fi
+    }
+    ithm_reload() {
+        history -c
+        history -r "$ITHM_HISTORY_FILE"
+    }
+    ithm_backup() {
+        local FILE=$1
+        if [ -z "$FILE" ]; then
+            FILE="$ITHM_HISTORY_FILE_NAME"
+        fi
+        if [ -n "$FILE" ] && [ -f "$ITHM_HISTORY_FILE_PATH$FILE" ]; then
+            local BACKUP_FOLDER="$ITHM_HISTORY_FILE_PATH${FILE}_backup/"
+            local BACKUP_PATH="$BACKUP_FOLDER$(date +%Y%m%d%H%M%S)"
+            [ -d "$BACKUP_FOLDER" ] || mkdir -p "$BACKUP_FOLDER"
+            cp $ITHM_HISTORY_FILE_PATH$FILE $BACKUP_PATH
+            echo "BACKUP CREATED: $BACKUP_PATH"
+        fi
+    }
+    ithm_backup_list() {
+        local FILE=$1
+        if [ -z "$FILE" ]; then
+            FILE="$ITHM_HISTORY_FILE_NAME"
+        fi
+        local BACKUP_FOLDER="$ITHM_HISTORY_FILE_PATH${FILE}_backup/"
+        if [ -n "$FILE" ] && [ -d "$BACKUP_FOLDER" ]; then
+            find $BACKUP_FOLDER -maxdepth 1 -type f -printf '%f\n' | sort -r
+        fi
+    }
+    ithm_backup_read() {
+        local FILE=$1
+        local BACKUP_FILE=$2
+        if [ -z "$FILE" ]; then
+            FILE="$ITHM_HISTORY_FILE_NAME"
+        fi
+        local BACKUP_PATH="$ITHM_HISTORY_FILE_PATH${FILE}_backup/$BACKUP_FILE"
+        if [ -f "$BACKUP_PATH" ]; then
+            $ITHM_FILE_READER $BACKUP_PATH
+        fi
+    }
+    ithm_backup_restore() {
+        local FILE=$1
+        local BACKUP_FILE=$2
+        local BACKUP_PATH="$ITHM_HISTORY_FILE_PATH${FILE}_backup/$BACKUP_FILE"
+        if [ -f "$BACKUP_PATH" ]; then
+            cp $BACKUP_PATH "$ITHM_HISTORY_FILE_PATH${FILE}.$BACKUP_FILE"
+        fi
+    }
+    ithm_open() {
+        if [ -z "$1" ]; then
+            nohup $ITHM_DIRECTORY_BROWSER "$ITHM_HISTORY_FILE_PATH" >/dev/null 2>&1
+        fi
+        if [ "$1" = "intellij" ]; then
+            nohup $ITHM_DIRECTORY_BROWSER "$(echo $INTELLIJ_ORIGINAL_PATH | grep -Eoh "(^.*/)")" >/dev/null 2>&1
+        fi
+    }
+    echo "CONSOLE $ITHM_HISTORY_FILE_NAME STARTED ITHM READY $([ ! -f "$ITHM_HISTORY_FILE" ] || echo "- history recovered")"
+    HISTFILE="$ITHM_HISTORY_FILE"
+    if [ -z "$ITHM_INITAL_USE_GLOBAL_HISTORY" ] && [ ! -f "$ITHM_HISTORY_FILE" ]; then
+        CLEAN_HISTORY=1
+    fi
+    [ -d "$ITHM_HISTORY_FILE" ] || mkdir -p $ITHM_HISTORY_FILE_PATH
+    [ -f "$ITHM_HISTORY_FILE" ] || touch $ITHM_HISTORY_FILE
+    if [ -n "$CLEAN_HISTORY" ]; then
+        history -c
+        history -w
+    fi
+fi
